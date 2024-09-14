520e6cb14e79cc19024d857be0a11acd58f709f5
Hunter Dobbelmann <hunterdobb@gmail.com>
2024-09-14 10:14:06 -0400

    refactor registration.php

--- smallprojectapi/registration.php
+++ smallprojectapi/registration.php
@@ -5,32 +5,32 @@ error_reporting(E_ALL);
 
 $inData = getRequestInfo();
 
-$FirstName = $inData["first_name"];
-$LastName = $inData["last_name"];
-$Email = $inData["email"];
-$Password = $inData["password"];
+$conn = require __DIR__ . "/database.php";
+$passwordHash = password_hash($inData["password"], PASSWORD_DEFAULT);
 
-$conn = new mysqli("localhost", "SmallAdmin", "GoodPassword", "smallproject");
+$stmt = $conn->prepare("INSERT into users (first_name, last_name, email, password) VALUES(?,?,?,?)");
+$stmt->bind_param(
+    "ssss",
+    $inData["first_name"],
+    $inData["last_name"],
+    $inData["email"],
+    $passwordHash
+);
+$stmt->execute();
 
-if ($conn->connect_error) {
-    returnWithError($conn->connect_error);
+$result = $stmt->get_result();
+
+if ($row = $result->fetch_assoc()) {
+    returnWithInfo($row['first_name'], $row['last_name'], $row['ID']);
 } else {
-    // $hashedPassword = password_hash($Password, PASSWORD_DEFAULT);
-    $stmt = $conn->prepare("INSERT into users (first_name, last_name, email, password) VALUES(?,?,?,?)");
-    $stmt->bind_param("ssss", $FirstName, $LastName, $Email, $Password);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    if ($row = $result->fetch_assoc()) {
-        returnWithInfo($row['first_name'], $row['last_name'], $row['ID']);
-    } else {
-        returnWithError("No Records Found");
-    }
-
-    $stmt->close();
-    $conn->close();
+    returnWithError("No Records Found");
 }
 
+$stmt->close();
+$conn->close();
+
+
+// Helper functions
 function returnWithInfo($firstName, $lastName, $id)
 {
     $retValue = '{"id":' . $id . ',"first_name":"' . $firstName . '","last_name":"' . $lastName . '","error":""}';
