235cbe25a6e4ee6b93b46fe57ca4d791be06b502
Hunter Dobbelmann <hunterdobb@gmail.com>
2024-09-22 15:08:11 -0400

    add finishing touches

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
-const urlBase = 'http://contacts.hunterdobb.xyz/smallprojectapi';
-const extension = 'php';
+let urlBase = 'http://contacts.hunterdobb.xyz/smallprojectapi';
+let extension = 'php';
 
 let userId = 0;
 let firstName = "";
@@ -55,10 +55,7 @@ function doLogin() {
 }
 
 function deleteContact() {
-  // let selectedContactID = document.getElementById("deleteContactID").value;
   let selectedContactID = selectedContact["id"];
-
-  // document.getElementById("deleteContactResult").innerHTML = "";
 
   let tmp = { id: selectedContactID };
   let jsonPayload = JSON.stringify(tmp);
@@ -72,20 +69,15 @@ function deleteContact() {
   try {
     xhr.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
-        // document.getElementById("deleteContactResult").innerHTML = "Contact has been deleted";
-        searchContacts(function() {
+        searchContacts(function () {
           selectedContact = "";
           selectFirstContactOrShowCreateContact();
         });
-        // selectedContact = "";
-        // let contactDetails = document.getElementById('contactDetails');
-        // contactDetails.innerHTML = "<h1>Successfully Deleted</h1>"
       }
     };
     xhr.send(jsonPayload);
   }
   catch (err) {
-    // document.getElementById("deleteContactResult").innerHTML = err.message;
   }
 }
 
@@ -110,7 +102,6 @@ function reselectCurrentContact() {
   } else {
     selectContactByID(selectedContact["id"]);
   }
-  
 }
 
 function readCookie() {
@@ -141,13 +132,10 @@ function readCookie() {
 }
 
 function selectFirstContactOrShowCreateContact() {
-  console.log("selectFirstContactOrShowCreateContact");
-  console.log(currentSearchResults);
-
   if (currentSearchResults.length == 0) {
-    const contactDetails = document.getElementById('contactDetails');
+    let contactDetails = document.getElementById('contactDetails');
     contactDetails.innerHTML = `
-      <h1>No Contacts. Click "+ New Contact" to create a contact.</h1>
+      <h1>No Contacts Found</h1>
   `;
   } else {
     let firstContact = currentSearchResults[0];
@@ -157,7 +145,7 @@ function selectFirstContactOrShowCreateContact() {
 
 // Function to handle selection of a contact
 function selectContact(contact) {
-  const items = document.querySelectorAll('.list-group-item');
+  let items = document.querySelectorAll('.list-group-item');
   items.forEach(item => item.classList.remove('active'));
   let selectedElement = document.getElementById(`contactID${contact["id"]}`);
   selectedElement.classList.add('active');
@@ -165,44 +153,85 @@ function selectContact(contact) {
   selectedContact = contact;
   console.log(`Setting seleced contatc to ${contact}`);
 
-  const contactDetails = document.getElementById('contactDetails');
+  let contactDetails = document.getElementById('contactDetails');
   contactDetails.innerHTML = `
     <button id="editContactButton" class="btn btn-outline-primary btn-sm" onclick=editSelectedContact()>Edit</button>
     </br></br>
-    <h1>${contact["first_name"]} ${contact["last_name"]}</h1>
-    </br></br>
-    <p><strong>Phone Number:</strong> ${contact["phone_number"]}</p>
-    <p><strong>Email:</strong> ${contact["email"]}</p>
+
+    <h1 style="display: flex; align-items: center;">
+    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+    </svg>
+    <span style="margin-left: 10px;">${contact["first_name"]} ${contact["last_name"]}</span>
+</h1>
+
+    </br>
+
+    <h5 style="display: flex; align-items: center; margin-left: 50px;">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" 
+        class="bi bi-telephone-fill" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+    </svg>
+    <span style="margin-left: 10px;">${contact["phone_number"]}</span>
+</h5>
+
+      </br>
+
+      <h5 style="display: flex; align-items: center; margin-left: 50px;">
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" 
+      class="bi bi-envelope-fill" viewBox="0 0 16 16">
+      <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"/>
+    </svg>
+    <span style="margin-left: 10px;">${contact["email"]}</span>
+</h5>
   `;
 }
 
 function createNewContact() {
+  let search = document.getElementById("searchText");
+  
+  if (search.value.length != 0) {
+    search.value = "";
+    searchContacts(null, false);
+  }
+  
   let contactDetails = document.getElementById('contactDetails');
   contactDetails.innerHTML = `
   <button id="cancelEditButton" class="btn btn-outline-primary btn-sm" onclick=reselectCurrentContact()>Cancel</button>
   <button id="saveContactButton" class="btn btn-primary btn-sm" onclick=addContact()><strong>Create</strong></button>
 
-  </br></br>
+  <main class="form-addcontact w-100">
+  <form>
+    <p class="text-danger text-center" id="addContactResult"></p> 
 
-  <div class="form-floating">
-    <input type="text" class="form-control" id="firstNameInput" placeholder="John"">
-    <label for="firstNameInput">First Name</label>
-  </div>
+  <div class="row gx-0">
+    <div class="col">
+      <div class="form-floating">
+        <input type="text" class="form-control" id="firstNameInput" placeholder="John"">
+        <label for="firstNameInput">First Name</label>
+        </div>
+    </div>
 
-  <div class="form-floating">
-    <input type="text" class="form-control" id="lastNameInput" placeholder="Doe">
-    <label for="lastNameInput">Last Name</label>
-  </div>
+    <div class="col">
+      <div class="form-floating">
+        <input type="text" class="form-control" id="lastNameInput" placeholder="Doe">
+        <label for="lastNameInput">Last Name</label>
+      </div>
+    </div>
+    </div>
 
-  <div class="form-floating">
-    <input type="email" class="form-control" id="phoneInput" placeholder="1234567890">
-    <label for="phoneInput">Phone Number</label>
-  </div>
+    <div class="form-floating">
+      <input type="email" class="form-control" id="phoneInput" placeholder="1234567890">
+      <label for="phoneInput">Phone Number</label>
+    </div>
 
-  <div class="form-floating">
-    <input type="email" class="form-control" id="emailInput" placeholder="name@example.com">
-    <label for="emailInput">Email address</label>
-  </div>
+    <div class="form-floating">
+      <input type="email" class="form-control" id="emailInput" placeholder="name@example.com">
+      <label for="emailInput">Email address</label>
+    </div>
+  </form>
+  </main>
   `;
 }
 
@@ -212,7 +241,7 @@ function addContact() {
   let updatedContactEmail = document.getElementById("emailInput").value;
   let updatedContactPhoneNumber = document.getElementById("phoneInput").value;
 
-  // document.getElementById("addContactResult").innerHTML = "";
+  document.getElementById("addContactResult").innerHTML = "";
 
   let tmp = { owner: userId, first_name: updatedContactFirstName, last_name: updatedContactLastName, email: updatedContactEmail, phone_number: updatedContactPhoneNumber };
   let jsonPayload = JSON.stringify(tmp);
@@ -230,10 +259,10 @@ function addContact() {
         let jsonObject = JSON.parse(xhr.responseText);
         let addedID = jsonObject["id"];
         console.log(`Added with ID: ${addedID}`)
-        // document.getElementById("addContactResult").innerHTML = "Contact has been added";
+        document.getElementById("addContactResult").innerHTML = "Contact has been added";
         // Add the new contact to the list
-        const contactList = document.getElementById('contactList');
-        const newContact = document.createElement('a');
+        let contactList = document.getElementById('contactList');
+        let newContact = document.createElement('a');
         newContact.href = "#";
         newContact.className = "list-group-item list-group-item-action py-3 lh-sm";
         newContact.innerHTML = `
@@ -244,15 +273,14 @@ function addContact() {
     `;
         newContact.onclick = function () { selectContact(newContact) };
         contactList.appendChild(newContact);
-        searchContacts(function() {
+        searchContacts(function () {
           selectContactByID(addedID);
         });
       }
     };
     xhr.send(jsonPayload);
-  }
-  catch (err) {
-    // document.getElementById("addContactResult").innerHTML = err.message;
+  } catch (err) {
+    document.getElementById("addContactResult").innerHTML = "Please fill out all fields and try again.";
   }
 }
 
@@ -269,33 +297,41 @@ function editSelectedContact() {
 
   // Replace text with input fields
   contactDetails.innerHTML = `
-  <button id="cancelEditButton" class="btn btn-outline-primary btn-sm" onclick=reselectCurrentContact()>Cancel</button>
+<button id="cancelEditButton" class="btn btn-outline-primary btn-sm" onclick=reselectCurrentContact()>Cancel</button>
   <button id="saveContactButton" class="btn btn-primary btn-sm" onclick=updateContact()><strong>Save</strong></button>
 
-  </br></br>
+  <main class="form-addcontact w-100">
+  <form>
+    <p class="text-danger text-center" id="editContactResult"></p> 
 
-  <div class="form-floating">
-    <input type="text" class="form-control" id="firstNameInput" placeholder="John" value="${selectedContact["first_name"]}">
-    <label for="firstNameInput">First Name</label>
-  </div>
+  <div class="row gx-0">
+    <div class="col">
+      <div class="form-floating">
+        <input type="text" class="form-control" id="firstNameInput" placeholder="John" value="${selectedContact["first_name"]}">
+        <label for="firstNameInput">First Name</label>
+      </div>
+    </div>
 
-  <div class="form-floating">
-    <input type="text" class="form-control" id="lastNameInput" placeholder="Doe" value="${selectedContact["last_name"]}">
-    <label for="lastNameInput">Last Name</label>
-  </div>
+    <div class="col">
+      <div class="form-floating">
+      <input type="text" class="form-control" id="lastNameInput" placeholder="Doe" value="${selectedContact["last_name"]}">
+      <label for="lastNameInput">Last Name</label>
+      </div>
+    </div>
+    </div>
 
-  <div class="form-floating">
-    <input type="email" class="form-control" id="phoneInput" placeholder="1234567890" value="${selectedContact["phone_number"]}">
-    <label for="phoneInput">Phone Number</label>
-  </div>
+    <div class="form-floating">
+      <input type="email" class="form-control" id="phoneInput" placeholder="1234567890" value="${selectedContact["phone_number"]}">
+      <label for="phoneInput">Phone Number</label>
+    </div>
 
-  <div class="form-floating">
-    <input type="email" class="form-control" id="emailInput" placeholder="name@example.com" value="${selectedContact["email"]}">
-    <label for="emailInput">Email address</label>
-  </div>
-
+    <div class="form-floating">
+      <input type="email" class="form-control" id="emailInput" placeholder="name@example.com" value="${selectedContact["email"]}">
+      <label for="emailInput">Email address</label>
+    </div>
+  </form>
+  </main>
   </br>
-
   <button id="deleteContactButton" class="btn btn-outline-danger btn-sm" onclick=deleteContact()>Delete</button>
   `;
 }
@@ -306,8 +342,6 @@ function updateContact() {
   let updatedContactLastName = document.getElementById("lastNameInput").value;
   let updatedContactEmail = document.getElementById("emailInput").value;
   let updatedContactPhoneNumber = document.getElementById("phoneInput").value;
-
-  // document.getElementById("updateContactResult").innerHTML = "";
 
   let tmp = { id: selectedContactID, first_name: updatedContactFirstName, last_name: updatedContactLastName, email: updatedContactEmail, phone_number: updatedContactPhoneNumber };
   let jsonPayload = JSON.stringify(tmp);
@@ -321,10 +355,9 @@ function updateContact() {
   try {
     xhr.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
-        // document.getElementById("updateContactResult").innerHTML = "Contact has been updated";
         console.log("Contact has been updated");
 
-        searchContacts(function() {
+        searchContacts(function () {
           reselectCurrentContact();
         });
       }
@@ -339,7 +372,7 @@ function updateContact() {
 }
 
 
-function searchContacts(callback = null) {
+function searchContacts(callback = null, selectFirst = true) {
   let search = document.getElementById("searchText").value;
 
   // Create a document fragment
@@ -358,11 +391,22 @@ function searchContacts(callback = null) {
     xhr.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
         let jsonObject = JSON.parse(xhr.responseText);
-        currentSearchResults = jsonObject.results;
-
+        
         if (jsonObject.results.length == 0) {
           // Optionally handle the case where no contacts are found
+          let contactList = document.getElementById('contactList');
+          contactList.innerHTML = "";
+
+          contactList.innerHTML = `<p style="text-align: center;">No Contacts Found</p>`;
+          currentSearchResults = "";
+
+          let contactDetails = document.getElementById('contactDetails');
+          contactDetails.innerHTML = `
+            <h1>No Contacts Found</h1>
+          `;
         } else {
+          currentSearchResults = jsonObject.results;
+
           for (let i = 0; i < jsonObject.results.length; i++) {
             let result = jsonObject.results[i];
             let foundContact = document.createElement('a');
@@ -387,6 +431,12 @@ function searchContacts(callback = null) {
 
           // Append the fragment to the actual container
           contactList.appendChild(fragment);
+
+          if (selectFirst) {
+            selectFirstContactOrShowCreateContact();
+          } else {
+            console.log("Don't select first");
+          }
 
           if (callback) {
             console.log(currentSearchResults);
